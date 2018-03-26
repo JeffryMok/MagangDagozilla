f0b41ddf9fd225ea4f3549d4d84c8fff2e190ffa
naufalzhafran <latifnaufal7@gmail.com>
2018-03-26 00:31:27 -0700

    Baru coba

---
+++ Zhafran/main.cpp
@@ -0,0 +1,16 @@
+#include "point.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  Point point;
+
+  point.x = 6;
+  point.y = 8;
+
+  printPoint(point);
+
+  return 0;
+}
---
+++ Zhafran/point.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "point.h"
+
+using namespace std;
+
+void printPoint(Point p){
+  cout << "(" << p.x << ", " << p.y << ")" <<endl;
+}
