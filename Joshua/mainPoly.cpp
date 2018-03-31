7d8f7add30a3eba1eb39d09bb338c1d3ace8931f
jrandiny <jrandiny2@gmail.com>
2018-03-31 12:42:38 +0700

    Update mainPoly to use Polygon object

--- Joshua/mainPoly.cpp
+++ Joshua/mainPoly.cpp
@@ -39,59 +39,40 @@ int main(){
     poly1.addPoint(p3);
     cetakPoly(poly1);
 
-    // cout << endl;
-    //
-    // cout << "delete point" << endl;
-    // delPoint(poly);
-    // cout << "current poly : " << endl;
-    // printPolygon(poly);
-    //
-    // cout << endl;
-    //
-    // Polygon poly2 = makePolygon(4);
-    //
-    // cout << "copying polygon" << endl;
-    // Polygon poly2(poly1);
-    // cout << "current poly : " << endl;
-    // poly1.print();
-    // cout << "current poly2 : " << endl;
-    // poly2.print();
-    //
-    // cout << endl;
-    //
-    //
-    // cout << "delete point from poly2" << endl;
-    // poly2.delPoint();
-    // cout << "current poly2   : " << endl;
-    // poly2.print();
-    //
-    // cout << endl;
-    //
-    // Point p4;
-    // p4.x = 0;
-    // p4.y = 0;
-    //
-    // cout << "add p4 to poly2 ";
-    // printPoint(p4);
-    // poly2.addPoint(p4);
-    // cout << "current poly : " << endl;
-    // poly1.print();
-    // cout << "current poly2 : " << endl;
-    // poly2.print();
-    //
-    // cout << endl;
+    cout << endl;
 
-    //
-    // cout << "add p3 to poly ";
-    // printPoint(p3);
-    // addPoint(p3, poly);
-    // cout << "current poly : " << endl;
-    // printPolygon(poly);
-    // cout << "current poly2 : " << endl;
-    // printPolygon(poly2);
-    //
-    // deletePolygon(poly);
-    // deletePolygon(poly2);
+    cout << "delete point" << endl;
+    poly1.delPoint();
+    cout << "current poly : " << endl;
+    cetakPoly(poly1);
 
+    cout << endl;
 
+    cout << "copying polygon" << endl;
+    Polygon poly2(poly1);
+    cetakPoly(poly1,poly2);
+
+    cout << endl;
+
+    cout << "delete point from poly2" << endl;
+    poly2.delPoint();
+    cetakPoly(poly1,poly2);
+
+    cout << endl;
+
+    Point p4(0,0);
+
+    cout << "add p4 to poly2 ";
+    p4.print();
+    poly2.addPoint(p4);
+    cetakPoly(poly1,poly2);
+
+    cout << endl;
+
+    cout << "add p3 to poly ";
+    p3.print();
+    poly1.addPoint(p3);
+    cetakPoly(poly1,poly2);
+
+    return 0;
 }
