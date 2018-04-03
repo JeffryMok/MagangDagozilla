6acdaf69ef62efcec1d2e317c6f5b9f596b2a201
tancos02 <paulussiahaan02@gmail.com>
2018-04-03 23:53:49 +0700

    Tugas ada di point.h dan point.cpp

--- Paulus/point.cpp
+++ Paulus/point.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Point add(Point p1, Point p2){
+/*Point add(Point p1, Point p2){
     Point p3;
     p3.x = p1.x + p2.x ;
     p3.y = p1.y + p2.y ;
@@ -52,4 +52,76 @@ bool is_origin(Point p){
 
 void printPoint(Point p){
 	cout << "(" << p.x << "," << p.y << ")" << endl;
+}
+*/
+Point::Point(){
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int x, int y){
+    this->x = x;
+    this->y = y;
+}
+
+Point Point::add(Point p){
+    Point sum;
+    sum.x = p.x + x;
+    sum.y = p.y + y;
+    return sum;
+}
+
+Point Point::substract(Point p){
+    Point hasil;
+    hasil.x = x - p.x;
+    hasil.y = y - p.y;
+    return hasil;
+}
+
+float Point::distance(Point p){
+    float jarak;
+    jarak = sqrt(((p.x - x)*(p.x -x)) + ((p.y - y)*(p.y - y)));
+    return jarak;
+}
+
+float Point::distance_to_origin(Point p){
+    float jarak;
+    jarak = sqrt((x*x)+(y*y));
+    return jarak;
+}
+
+int Point::quadrant(){
+    int kuadran;
+    if ((x==0)||(y==0)){
+        kuadran = 0;
+    }else{
+        if(x>0){
+            if(y>0){
+                kuadran = 1;
+            }else{
+                kuadran = 4;
+            }
+        }else{
+            if(y>0){
+                kuadran = 2;
+            }else{
+                kuadran = 3;
+            }
+        }
+    }
+
+
+    return kuadran;
+}
+
+bool Point::is_equal(Point p){
+    return ((x==p.x)&&(y==p.y));
+}
+
+bool Point::is_origin(){
+    return ((x==0)&&(y==0));
+}
+
+void Point::print(){
+    cout << "(" << x << "," << y << ")" << endl;
 }
--- Paulus/point.h
+++ Paulus/point.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
+/*using namespace std;
 typedef struct Point {
 	int x;
 	int y;
@@ -18,5 +18,23 @@ int quadrant(Point p);
 bool is_equal(Point p1, Point p2);
 bool is_origin(Point p);
 void printPoint(Point p);
+*/
+class Point{
+    private:
+        int x;
+        int y;
 
+    public:
+        point();
+        point(int x, int y);
+
+        Point add(Point p);
+        Point substract(Point p);
+        float distance(Point p);
+        float distance_to_origin();
+        int quadrant();
+        bool is_equal(Point p1);
+        bool is_origin();
+        void print();
+};
 #endif
